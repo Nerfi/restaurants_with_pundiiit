ccf79b341af4fb0d98892a8c9f1b4633429cabde
Juan Paredes <juanchuu.jfpc@gmail.com>
2019-02-15 11:45:53 +0100

    added dry to restaurant_policy and updated the new path with policy in the index page

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -3,7 +3,7 @@ class RestaurantPolicy < ApplicationPolicy
     def resolve
       scope.all # to let anyone see the restaurants
       #display only restaurants of owner
-      scope.where(user: user)
+      #scope.where(user: user)
     end
   end
 
@@ -32,8 +32,23 @@ class RestaurantPolicy < ApplicationPolicy
     #inside policy we need to remenber this two things
     # 1. user is currrent_user
     #2. record is the instance we passed when we authorize in this case record will be @restaurant
-    record.user == user
+    user_is_owner?
     # we can update a restaurant if we create it, that's what this line is telling us
+
+  end
+
+  def destroy?
+    #1 and admin
+
+    #2 the owner of the restaurant, who created the record
+    user_is_owner?
+
+  end
+
+  private
+  #hacemos esto para mantener el principio DRY line 44 and 35 repeat
+  def user_is_owner?
+    record.user == user
 
   end
 end
